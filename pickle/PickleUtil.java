bc3ba59d928ccb400c83919e87043e4314c306b5
patlopardi <patricklopardi@gmail.com>
2021-02-26 20:43:35 -0600

    added comment skipping function

--- pickle/PickleUtil.java
+++ pickle/PickleUtil.java
@@ -3,10 +3,21 @@ package pickle;
 import java.util.Arrays;
 
 public final class PickleUtil {
+
+    /**
+     * <p>
+     * This function checks for comments on each line of code and then deletes
+     *      them from the character array to be processed later in the scanner
+     *
+     * @param textArray  a character array that contains the character of a line in the code
+     *
+     * @return      char[]
+     */
     public static char[] checkComments(char[] textArray){
         // Loop through text array looking for double slash
+        boolean stringFlag = false;
         for(int i = 0; i < textArray.length - 1; i++){
-            if(textArray[i] == '/' && textArray[i + 1] == '/'){
+            if(textArray[i] == '/' && textArray[i + 1] == '/' && !stringFlag){
                 // if the // is at the beginning send an empty array back
                 if((i - 1) > 0){
                     // Cut the comments out of the token array
@@ -18,6 +29,10 @@ public final class PickleUtil {
                     textArray = new char[]{};
                     break;
                 }
+            }
+            else if (textArray[i] == '\"' || textArray[i] == '\'') {
+                // Swaps on when in string swaps off when meeting end quote
+                stringFlag = !stringFlag;
             }
         }
         return textArray;
